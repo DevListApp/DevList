426f47f3dd3b6a337556ed73871f176e1780eb10
Gabriel Mendonça <115747161+Gabriel037@users.noreply.github.com>
2023-12-14 20:10:23 -0300

    Initial commit

---
+++ app/src/main/java/com/devlist/app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.devlist.app;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.devlist.app.auth.LoginUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button viewRegister, viewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewRegister = findViewById(R.id.viewRegister);
+        viewLogin = findViewById(R.id.viewLogin);
+
+        viewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterUser.class);
+                startActivity(intent);
+                onDestroy();
+            }
+        });
+
+        viewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginUser.class);
+                startActivity(intent);
+                onDestroy();
+            }
+        });
+
+    }
+
+}
+
+
+
