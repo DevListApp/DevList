697c6b01eac434adfafc4ce87c1776a17476f696
bruna <brunagmaia05@gmail.com>
2024-01-08 23:39:07 -0300

    correção SplashScreen1

--- app/src/main/java/com/devlist/app/SplashScreen1.java
+++ app/src/main/java/com/devlist/app/SplashScreen1.java
@@ -12,6 +12,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreen1 extends AppCompatActivity {
+
+    //Criando variáveis
     ProgressBar splash_carregamento;
     Timer timer;
     int cont = 0;
@@ -23,12 +25,14 @@ public class SplashScreen1 extends AppCompatActivity {
 
         splash_carregamento = findViewById(R.id.splash_carregamento);
 
+        //Atualiza a barra de progresso e quando termina o tempo, muda para a SplashScreen2
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
+                //a cada 100 milissegundos splash_carregamento é incrementada em 1
                 cont++;
                 splash_carregamento.setProgress(cont);
-                if (cont == 100) {
+                if (cont == 30) {
                     startActivity(new Intent(SplashScreen1.this, SplashScreen2.class));
                     finish();
                     timer.cancel();
@@ -36,17 +40,8 @@ public class SplashScreen1 extends AppCompatActivity {
             }
         };
 
-
+        //Instancia o Timer e agenda a tarefa para ser executada a cada 100 milissegundos (0 inicialmente).
         timer = new Timer();
         timer.schedule(timerTask, 0, 100);
-
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen1.this, SplashScreen2.class));
-                finish();
-            }
-        }, 3000);
     }
 }
