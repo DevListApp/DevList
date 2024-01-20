03d73661b6752b15e3e9efb47ea5272d106da0b0
Gabriel Mendonça <115747161+Gabriel037@users.noreply.github.com>
2024-01-20 15:58:19 -0300

    feat: Foi adicionado componente data Picker para programar as tarefas mais refactor modelo MVVM

--- app/src/main/java/com/devlist/app/screens/task/CreateTask.java
+++ app/src/main/java/com/devlist/app/screens/task/CreateTask.java
@@ -1,10 +1,13 @@
 package com.devlist.app.screens.task;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,16 +15,24 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.devlist.app.R;
+import com.devlist.app.data.repositories.TaskRepository;
+import com.devlist.app.data.sources.TaskFirebaseDataSource;
+import com.devlist.app.data.sources.UserFirebaseDataSource;
 import com.devlist.app.screens.dashboard.Dashboard;
 import com.devlist.app.data.models.Task;
+import com.devlist.app.screens.user.RegisterUser;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateTask extends AppCompatActivity {
 
@@ -30,7 +41,10 @@ public class CreateTask extends AppCompatActivity {
     private Button btnAdicionarTarefa, btnBackAddTask; // BOTAO ADICIONA TAREFA
     private FirebaseFirestore databaseReference;
     private FirebaseAuth myAuth;
-
+    private DatePickerDialog picker;
+    private Date dataInicial;
+    private Date dataFinal;
+    private TaskRepository taskRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +66,7 @@ public class CreateTask extends AppCompatActivity {
         btnAdicionarTarefa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                adicionarTarefa();
+                adicionarTarefa( edtTituloTarefa.getText().toString().trim(),  edtNotasTarefa.getText().toString().trim(), dataInicial, dataFinal );
             }
         });
         btnBackAddTask.setOnClickListener(new View.OnClickListener() {
@@ -63,24 +77,51 @@ public class CreateTask extends AppCompatActivity {
                 finish();
             }
         });
+        edtHoraInicio.setInputType(InputType.TYPE_NULL);
+        edtHoraInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDate(edtHoraInicio);
+            }
+        });
+        edtHoraFim.setInputType(InputType.TYPE_NULL);
+        edtHoraFim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showDate(edtHoraFim);
+            }
+
+        });
     }
+    public void showDate(EditText dateButton){
+        Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+        picker = new DatePickerDialog(CreateTask.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        dateButton.setText(dayOfMonth + "/" + (((monthOfYear + 1) < 10) ? ("0" + (monthOfYear + 1)) : (monthOfYear + 1)) + "/" + year);
+                        Calendar selectedDate = Calendar.getInstance();
+                        // Atualizar o valor da variável de instância
+                        if (dateButton == edtHoraInicio) {
+                            selectedDate.set(year, monthOfYear, dayOfMonth, 0, 0, 0);
+                            dataInicial = selectedDate.getTime();
+                        } else if (dateButton == edtHoraFim) {
+                            selectedDate.set(year, monthOfYear, dayOfMonth, 23, 59, 59);
+                            dataFinal = selectedDate.getTime();
+                        }
+                    }
+                }, year, month, day);
+        picker.show();
+    };
 
-    private void adicionarTarefa() {
-        // OBTENDO INFORMACOES DA INTERFACE (TELA) DOS DADOS INFORMADOS PELO USUARIO
-        String titulo = edtTituloTarefa.getText().toString().trim();
-        String notas = edtNotasTarefa.getText().toString().trim();
-        String horaInicio = edtHoraInicio.getText().toString().trim();
-        String horaFim = edtHoraFim.getText().toString().trim();
-
-
-        // LOG PARA TESTAR OS DADOS ENVIADOS
-        Log.d("DEBUG", "Titulo: " + titulo);
-        Log.d("DEBUG", " Notas: " + notas);
-        Log.d("DEBUG", "Hora Inicio: " + horaInicio);
-        Log.d("DEBUG", "Hora Fim: " + horaFim);
-
+    private void adicionarTarefa(String titulo, String notas, Date dataInicio, Date dataFinal ) {
+        taskRepository = new TaskRepository();
         // VERIFICA SE TODOS OS CAMPOS ESTÃO PREENCHIDOS
-        if (titulo.isEmpty() || horaInicio.isEmpty() || horaFim.isEmpty()) {
+        if (titulo.isEmpty() || dataInicio == null || dataFinal == null) {
             Toast.makeText(this, "Preencha todos os campos obrigatórios", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -92,40 +133,29 @@ public class CreateTask extends AppCompatActivity {
         // LOG TESTAR
         Log.d("DEBUG", "Prioridade Selecionada: " + prioridadeSelecionada.getText().toString());
 
+        // CHAMANDO INSTANCIA DA CLASSE TAREFAS
+        Task tarefa = new Task("", titulo, notas, dataInicio, dataFinal, getCodigoPrioridade(prioridadeSelecionada.getText().toString()), getUid(), 0);
+        taskRepository.createTask(tarefa , new TaskFirebaseDataSource.TaskListener(){
+            @Override
+            public void onSuccess() {
+                Toast.makeText(CreateTask.this, "Tarefa adicionada com sucesso", Toast.LENGTH_SHORT).show(); // ALERT
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+                finish(); // FECHA A ATIVIDADE APOS ADICIONAR A TAREFA
+            }
+            @Override
+            public void onFailure(String errorMessage) {
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public String getUid(){
         // PEGANDO O UID DO USUÁRIO
         myAuth = FirebaseAuth.getInstance();
         FirebaseUser user  = myAuth.getCurrentUser();
         String userId = user.getUid();
-
-        // CHAMANDO INSTANCIA DA CLASSE TAREFAS
-        Task tarefa = new Task(
-                titulo,
-                notas,
-                new Date(),
-                new Date(),
-                getCodigoPrioridade(prioridadeSelecionada.getText().toString()),
-                userId
-        );
-
-        // AO CLICAR, ENVIA OS DADOS PARA PODER SALVAR NO BANCO
-        databaseReference.collection("tasks").add(tarefa)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(CreateTask.this, "Tarefa adicionada com sucesso", Toast.LENGTH_SHORT).show(); // ALERT
-                        Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-                        startActivity(intent);
-                        finish(); // FECHA A ATIVIDADE APOS ADICIONAR A TAREFA
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(CreateTask.this, "Erro ao adicionar a tarefa: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        Log.e("TAG", "Erro ao adicionar a tarefa"+ e.getMessage()); // LOG ERRO
-                    }
-                });
-
+        return userId;
     }
 
     private int getCodigoPrioridade(String prioridade) {
